43fcfb91377ec8194096c7f31994f4af111a22f1
guilhermezanollo <guilherme.zanollo@gmail.com>
2025-02-10 16:27:50 -0300

    Primeira atualização de projeto

---
+++ QuintoProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuintoProjeto
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            int M, N;
+            int[,] A;
+
+            M = int.Parse(Console.ReadLine());
+            N = int.Parse(Console.ReadLine());
+            A = new int[M, N];
+        }
+    }
+}
